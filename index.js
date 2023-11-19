6e45ef1a9c5152d8693f6e4e39526eac03f93a94
HoangChien201 <hoangchien11522@gmail.com>
2023-11-19 18:24:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const cors=require('cors');
+const mongoose=require('mongoose')
+let bodyParser=require('body-parser');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
+
+//router
+
+
+const PORT=process.env.PORT||3000;
+
+const app=express();
+const uri="mongodb+srv://hoangchien11522:0368670025@cluster0.hqd4zon.mongodb.net/test?retryWrites=true&w=majority"
+
+
+try {
+    mongoose.set('strictQuery', true)
+    mongoose.connect(uri,()=>{
+    console.log('Mongo connected')
+    })
+} catch(error) {
+    console.log(error)
+    process.exit()
+}
+
+app.use(bodyParser.json({"limit":"50mb"}))
+app.use(cors());
+app.use(morgan("common"));
+
+// app.use("/staff",staffRouter);
+
+app.listen(PORT,()=>{
+    console.log("Server is running ...");
+})
