934fbf0b63d6c7ffd7460c55e566b6ba3262bb62
= <=>
2023-11-28 20:37:50 +0700

    up

--- controller/userController.js
+++ controller/userController.js
@@ -1,93 +1,92 @@
-const {User} = require('../model/user')
+const { User } = require('../model/user')
 
-const userController={
-    getUser:async (req,res)=>{
+const userController = {
+    getUser: async (req, res) => {
         try {
-            const users= await User.find()
+            const users = await User.find()
             return res.status(200).json(users)
         } catch (error) {
             return res.status(500).json(error)
         }
-       
+
 
     },
-    getUsers:async(req,res) =>{
-       try {
-        const user = await User.find({}, 'email score');
-        return res.status(200).json(user)
-       } catch (error) {
-        return res.status(500).json(error)
-       }
-    },
-     login: async(  req , res) =>{
+    getUsers: async (req, res) => {
         try {
-            const {email,password} = req.body;
-            const user = await User.findOne({email:email});
-            if(!user) throw new Error("Khong tim thaay tai khoang");
-            if(user.password != password) throw new Error("mat khau khong dung")
+            const user = await User.find({}, 'email score');
             return res.status(200).json(user)
         } catch (error) {
-            console.log("Lỗi",error);
-            throw new Error("lỗi rồi",error)
+            return res.status(500).json(error)
         }
-    
     },
-     changePassword :async (req,res)=>{
+    login: async (req, res) => {
         try {
-            const {email,newPassword,oldPassword} = req.body;
-            const user = await User.findOne({email:email});
-            if(!user) return res.status(500).json({message:"not find account"});
-            if(user.password != oldPassword){
-                return res.status(500).json({message:"password not trung"})
-    
-            }else{
+            const { email, password } = req.body;
+            const user = await User.findOne({ email: email });
+            if (!user) throw new Error("Khong tim thaay tai khoang");
+            if (user.password != password) throw new Error("mat khau khong dung")
+            return res.status(200).json(user)
+        } catch (error) {
+            console.log("Lỗi", error);
+            throw new Error("lỗi rồi", error)
+        }
+
+    },
+    changePassword: async (req, res) => {
+        try {
+            const { email, newPassword, oldPassword } = req.body;
+            const user = await User.findOne({ email: email });
+            if (!user) return res.status(500).json({ message: "not find account" });
+            if (user.password != oldPassword) {
+                return res.status(500).json({ message: "password not trung" })
+
+            } else {
                 user.password = newPassword;
             }
             // await user.updateOne({password:newPassword});
             return res.status(200).json("change-password success");
         } catch (error) {
-            console.log("Loi",error);
-            return res.status(400).json({message:error.message});
+            console.log("Loi", error);
+            return res.status(400).json({ message: error.message });
         }
     },
-    register : async (req,res) => {
+    register: async (req, res) => {
         try {
-            const {email,password} = req.body;
-            const register =  User.create({
-                email:email,
-                password:password
+            const { email, password } = req.body;
+            const register = User.create({
+                email: email,
+                password: password
             })
             await register;
             console.log(register);
-           
-            return res.status(200).json({message:"Sucessfully",register})
+
+            return res.status(200).json({ message: "Sucessfully", register })
         } catch (error) {
-            console.log("loi",error);
-            return res.status(500).json({message:error.message});
+            console.log("loi", error);
+            return res.status(500).json({ message: error.message });
         }
     },
-     saveScore : async(req,res) =>{
+    saveScore: async (req, res) => {
         try {
-            const {email,score} = req.body;
-            const saveScore = await User.findOne({email:email}).updateOne({score:score});
-            return res.status(200).json({message:"thành công"})
+            const { email, score } = req.body;
+            const saveScore = await User.findOne({ email: email }).updateOne({ score: score });
+            return res.status(200).json({ message: "thành công" })
         } catch (error) {
-            console.log("Loi",error);
-            throw new Error("Loi",error);
+            console.log("Loi", error);
+            throw new Error("Loi", error);
         }
     },
-     savePosition : async(req,res)=>{
+    savePosition: async (req, res) => {
         try {
-            const {positionX,positionZ,positionY,email} = req.body;
-            const savePosition = await User.findOne({email:email}).updateOne({positionX:positionX,positionY:positionY,positionZ:positionZ});
-            return res.status(200).json({message:"Thành công"})
+            await User.findOne({ email: email }).updateOne(req.body);
+            return res.status(200).json({ message: "Thành công" })
         } catch (error) {
-            console.log("loi",error);
-            return res.status.json({message:"Lỗi"})
+            console.log("loi", error);
+            return res.status.json({ message: "Lỗi" })
         }
     },
-    
+
 
 }
 
-module.exports=userController;
+module.exports = userController;
--- model/user.js
+++ model/user.js
@@ -3,18 +3,18 @@ const mongoose = require("mongoose");
 const userShema = new mongoose.Schema({
     email: {
         type: String,
-        require:true
+        require: true
     },
-    password:{
+    password: {
         type: String,
-        require:true
+        require: true
     },
-    score: { type: Number,require:false },
-    positionX: { type: Number,require:false},
-    positionY: { type: Number,require:false },
-    positionZ: { type: Number,require:false },
-    scences:{type:String,require:false}
-    
+    score: { type: Number, require: false },
+    positionX: { type: Number, require: false },
+    positionY: { type: Number, require: false },
+    positionZ: { type: Number, require: false },
+    scenes: { type: String, require: false }
+
 })
 
 const User = mongoose.model("User", userShema);
